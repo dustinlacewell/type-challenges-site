b59fe2f8eb72204aad5841101f6a39f9938ee7af
Dustin Lacewell <dlacewell@gmail.com>
2021-05-07 02:00:50 -0500

    initial commit

---
+++ apps/type-tetris/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'type-tetris',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/type-tetris',
+};
